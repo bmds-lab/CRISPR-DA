628ee1eb8ddc245ca8083a82bd9d01e310390662
Carl Schmitz <carl.schmitz@connect.qut.edu.au>
2025-02-17 19:43:59 +1000

    Updated documentation and refactors for consistency

--- viralcut/on-target.py
+++ viralcut/on-target.py
@@ -3,7 +3,7 @@ on-target.py
 
 This file contains methods for assessing a CRISPR guide RNA's on-target effiency.
 CRISPR DeepEnsemble can be found here: https://github.com/bmds-lab/CRISPR_DeepEnsemble 
-Crackling can be found here: https://github.com/bmds-lab/Crackling
+Crackling can be found here: https://github.com/bmds-lab/Crackling/blob/9e9d78196e97fe11e60f0d9bcc7c7e1349a03ae4/src/crackling/Crackling.py
 '''
 
 from tempfile import NamedTemporaryFile
@@ -45,7 +45,18 @@ t.manual_seed(123)
 dtype = t.float64
 t.set_default_dtype(dtype)
 
-def run_crispr_deep_ensemble(candidate_guides, score_threshold=0.7, uncertainty_threshold=0.05):
+def run_CRISPR_DeepEnsemble(collection, score_threshold=0.7, uncertainty_threshold=0.05):
+    '''
+    Utilising the model from https://github.com/bmds-lab/CRISPR_DeepEnsemble to predict on-target efficiency.
+    'This allows to design strategies that consider uncertainty in guide RNA selection'.
+
+    Arguments:
+    collection (ViralCut.Collection): A ViralCut collection containing the candidate guides.
+    score_threshold (float): A score threshold that must be exceed for a candidate guide to be 'efficient'. 
+                             Must be in the range from 0 to 1.
+    uncertainty_threshold (float): A uncertainty threshold that must not be exceed for a score to be considered 'acceptable'. 
+                                   Must be in the range from 0 to 1.
+    '''
     # NOTE
     # CDE = CRISPR Deep Ensemble
     # UQ = Uncertainty Quantification 
@@ -61,12 +72,12 @@ def run_crispr_deep_ensemble(candidate_guides, score_threshold=0.7, uncertainty_
 
     # Encode data and extract features
     oneHot = []
-    for guide in candidate_guides:
-        for target30 in candidate_guides[guide]['30mer']:
+    for guide in collection:
+        for target30 in collection[guide]['30mer']:
             oneHot.append(np.array(utils.one_hot_encode(target30)).tolist())
     meltingPoint = []
-    for guide in candidate_guides:
-        for target30 in candidate_guides[guide]['30mer']:
+    for guide in collection:
+        for target30 in collection[guide]['30mer']:
             myseq = Seq(target30)
             meltingPoint.append(mt.Tm_NN(myseq))
     _onehot = t.tensor(oneHot).transpose(1,2).unsqueeze(dim=1) 
@@ -76,70 +87,71 @@ def run_crispr_deep_ensemble(candidate_guides, score_threshold=0.7, uncertainty_
     prediction = ensemble.predict(inputs = (_onehot, _meltingpoint)).tolist()
     UQ = ensemble.uncertainty_bounds(inputs = (_onehot, _meltingpoint), n_samples=1000, lower=0.01, upper=0.99)
 
-    for guide in candidate_guides:
-        for target30 in candidate_guides[guide]['30mer']:
+    for guide in collection:
+        for target30 in collection[guide]['30mer']:
             score = prediction.pop(0)
             UQLowerBound, UQUpperBound, UQInterquartileRange = UQ.pop(0)
             UQRange = UQUpperBound - UQLowerBound
-            if hasattr(candidate_guides[guide], 'CDE_score'):
-                candidate_guides[guide]['CDE_score'].append(score)
-                candidate_guides[guide]['CDE_UQ_range'].append(UQRange)
-                candidate_guides[guide]['CDE_UQ_IQR'].append(UQInterquartileRange)
+            if hasattr(collection[guide], 'CDE_score'):
+                collection[guide]['CDE_score'].append(score)
+                collection[guide]['CDE_UQ_range'].append(UQRange)
+                collection[guide]['CDE_UQ_IQR'].append(UQInterquartileRange)
                 if score > score_threshold and UQRange < UQ_threshold:
-                    candidate_guides[guide]['CDE_passed'].append(True)
+                    collection[guide]['CDE_passed'].append(True)
                 else:
-                    candidate_guides[guide]['CDE_passed'].append(False)
+                    collection[guide]['CDE_passed'].append(False)
             else:
-                candidate_guides[guide]['CDE_score'] = [score]
-                candidate_guides[guide]['CDE_UQ_range'] = [UQRange]
-                candidate_guides[guide]['CDE_UQ_IQR'] = [UQInterquartileRange]
+                collection[guide]['CDE_score'] = [score]
+                collection[guide]['CDE_UQ_range'] = [UQRange]
+                collection[guide]['CDE_UQ_IQR'] = [UQInterquartileRange]
                 if score > score_threshold and UQRange < UQ_threshold:
-                    candidate_guides[guide]['CDE_passed'] = [True]
+                    collection[guide]['CDE_passed'] = [True]
                 else:
-                    candidate_guides[guide]['CDE_passed'] = [False]
+                    collection[guide]['CDE_passed'] = [False]
 
 
 
-def run_mini_crackling(candidate_guides):
-    '''This is a minimised version of Crackling, based on:
-        https://github.com/bmds-lab/Crackling/blob/9e9d78196e97fe11e60f0d9bcc7c7e1349a03ae4/src/crackling/Crackling.py
+def run_crackling_on_target(collection):
+    '''
+    A subset of the Crackling method that scores the on-target efficiency.
+    Based on: https://github.com/bmds-lab/Crackling/blob/9e9d78196e97fe11e60f0d9bcc7c7e1349a03ae4/src/crackling/Crackling.py.
 
     Arguments:
-        candidate_guides (ViralCutCollection):  A ViralCutCollection of candidate CRISPR-Cas9 sgRNA to score
+        collection (ViralCut.Collection): A ViralCut collection containing the candidate guides.
     '''
 
     ## G20
-    for target23 in candidate_guides:
+    for target23 in collection:
         if target23[19] != 'G':
-            candidate_guides[target23]['passed_g20'] = CODE_REJECTED
+            collection[target23]['passed_g20'] = CODE_REJECTED
         else:
-            candidate_guides[target23]['passed_g20'] = CODE_ACCEPTED
+            collection[target23]['passed_g20'] = CODE_ACCEPTED
 
     ## Removing targets with leading T
-    for target23 in candidate_guides:
+    for target23 in collection:
         if (target23[-2:] == 'GG' and target23[0] == 'T') or \
             (target23[:2] == 'CC' and target23[-1] == 'A'):
-            candidate_guides[target23]['passed_avoid_leading_t'] = CODE_REJECTED
+            collection[target23]['passed_avoid_leading_t'] = CODE_REJECTED
         else:
-            candidate_guides[target23]['passed_avoid_leading_t'] = CODE_ACCEPTED
+            collection[target23]['passed_avoid_leading_t'] = CODE_ACCEPTED
 
     ## AT% ideally is between 20-65%
-    for target23 in candidate_guides:
+    for target23 in collection:
         at_percentage = float(sum([x.upper() in ['A', 'T'] for x in target23[0:20]]) / 20.0 * 100.0)
 
         if at_percentage < 20 or at_percentage > 65:
-            candidate_guides[target23]['passed_at_percent'] = CODE_REJECTED
+            collection[target23]['passed_at_percent'] = CODE_REJECTED
         else:
-            candidate_guides[target23]['passed_at_percent'] = CODE_ACCEPTED
+            collection[target23]['passed_at_percent'] = CODE_ACCEPTED
 
-        candidate_guides[target23]['AT'] = at_percentage
+        collection[target23]['AT'] = at_percentage
 
     ## Removing targets that contain TTTT
-    for target23 in candidate_guides:
+    for target23 in collection:
         if 'TTTT' in target23:
-            candidate_guides[target23]['passed_tttt'] = CODE_REJECTED
+            collection[target23]['passed_tttt'] = CODE_REJECTED
         else:
-            candidate_guides[target23]['passed_tttt'] = CODE_ACCEPTED
+            collection[target23]['passed_tttt'] = CODE_ACCEPTED
 
     ## Calculating secondary structures
     guide = 'GUUUUAGAGCUAGAAAUAGCAAGUUAAAAUAAGGCUAGUCCGUUAUCAACUUGAAAAAGUGGCACCGAGUCGGUGCUUUU'
@@ -150,7 +162,7 @@ def run_mini_crackling(candidate_guides):
         os.remove(config['rnafold']['output'])
 
     with open(config['rnafold']['input'], 'w+') as f_rna_input:
-        for target23 in candidate_guides:
+        for target23 in collection:
             f_rna_input.write(f'G{target23[1:20]}{guide}\n')
 
     subprocess.run(
@@ -185,7 +197,7 @@ def run_mini_crackling(candidate_guides):
             i += 1
 
 
-    for target23 in candidate_guides:
+    for target23 in collection:
         key = target23[1:20]
 
         if key not in rna_structures:
@@ -198,41 +210,41 @@ def run_mini_crackling(candidate_guides):
         structure = line_2.split(' ')[0]
         energy = line_2.split(' ')[1][1:-1]
 
-        candidate_guides[target23]['ss_line_1'] = line_1
-        candidate_guides[target23]['ss_structure'] = structure
-        candidate_guides[target23]['ss_energy'] = energy
+        collection[target23]['ss_line_1'] = line_1
+        collection[target23]['ss_structure'] = structure
+        collection[target23]['ss_energy'] = energy
 
         if utils.trans_to_dna(target) != target23[0:20] and utils.trans_to_dna('C'+target[1:]) != target23[0:20] and utils.trans_to_dna('A'+target[1:]) != target23[0:20]:
-            candidate_guides[target23]['passed_secondary_structure'] = CODE_ERROR
+            collection[target23]['passed_secondary_structure'] = CODE_ERROR
             continue
 
         match_structure = re.search(pattern_rna_structure, line_2)
         if match_structure:
             energy = ast.literal_eval(match_structure.group(1))
             if energy < float(config['rnafold']['low_energy_threshold']):
-                candidate_guides[utils.trans_to_dna(target23)]['passed_secondary_structure'] = CODE_REJECTED
+                collection[utils.trans_to_dna(target23)]['passed_secondary_structure'] = CODE_REJECTED
             else:
-                candidate_guides[target23]['passed_secondary_structure'] = CODE_ACCEPTED
+                collection[target23]['passed_secondary_structure'] = CODE_ACCEPTED
         else:
             match_energy = re.search(pattern_rna_energy, line_2)
             if match_energy:
                 energy = ast.literal_eval(match_energy.group(1))
                 if energy <= float(config['rnafold']['high_energy_threshold']):
-                    candidate_guides[utils.trans_to_dna(target23)]['passed_secondary_structure'] = CODE_REJECTED
+                    collection[utils.trans_to_dna(target23)]['passed_secondary_structure'] = CODE_REJECTED
                 else:
-                    candidate_guides[target23]['passed_secondary_structure'] = CODE_ACCEPTED
+                    collection[target23]['passed_secondary_structure'] = CODE_ACCEPTED
 
     ## Calc mm10db result
-    for target23 in candidate_guides:
+    for target23 in collection:
         if not all([
-            candidate_guides[target23]['passed_at_percent'] == CODE_ACCEPTED,
-            candidate_guides[target23]['passed_tttt'] == CODE_ACCEPTED,
-            candidate_guides[target23]['passed_secondary_structure'] == CODE_ACCEPTED,
-            candidate_guides[target23]['passed_avoid_leading_t'] == CODE_ACCEPTED,
+            collection[target23]['passed_at_percent'] == CODE_ACCEPTED,
+            collection[target23]['passed_tttt'] == CODE_ACCEPTED,
+            collection[target23]['passed_secondary_structure'] == CODE_ACCEPTED,
+            collection[target23]['passed_avoid_leading_t'] == CODE_ACCEPTED,
         ]):
-            candidate_guides[target23]['accepted_by_mm10db'] = CODE_REJECTED
+            collection[target23]['accepted_by_mm10db'] = CODE_REJECTED
         else:
-            candidate_guides[target23]['accepted_by_mm10db'] = CODE_ACCEPTED
+            collection[target23]['accepted_by_mm10db'] = CODE_ACCEPTED
 
     ## sgRNAScorer 2.0 model
     encoding = {
@@ -245,7 +257,7 @@ def run_mini_crackling(candidate_guides):
     with importlib.resources.path('viralcut.resources', config['sgrnascorer2']['model']) as fp:
         clf_linear = joblib.load(fp) #config['sgrnascorer2']['model'])
 
-    for target23 in candidate_guides:
+    for target23 in collection:
         sequence = target23.upper()
         entry_list = []
 
@@ -256,19 +268,18 @@ def run_mini_crackling(candidate_guides):
         # predict based on the entry
         score = clf_linear.decision_function([entry_list])[0]
 
-        candidate_guides[target23]['sgrnascorer2_score'] = score
+        collection[target23]['sgrnascorer2_score'] = score
 
         if float(score) < float(float(config['sgrnascorer2']['score_threshold'])):
-            candidate_guides[target23]['accepted_by_sgrnascorer'] = CODE_REJECTED
+            collection[target23]['accepted_by_sgrnascorer'] = CODE_REJECTED
         else:
-            candidate_guides[target23]['accepted_by_sgrnascorer'] = CODE_ACCEPTED
+            collection[target23]['accepted_by_sgrnascorer'] = CODE_ACCEPTED
 
     ## Begin efficacy consensus
-    for target23 in candidate_guides:
-        candidate_guides[target23]['consensus_count'] = sum([
-            candidate_guides[target23]['accepted_by_mm10db'] == CODE_ACCEPTED,
-            candidate_guides[target23]['accepted_by_sgrnascorer'] == CODE_ACCEPTED,
-            candidate_guides[target23]['passed_g20'] == CODE_ACCEPTED,
+    for target23 in collection:
+        collection[target23]['consensus_count'] = sum([
+            collection[target23]['accepted_by_mm10db'] == CODE_ACCEPTED,
+            collection[target23]['accepted_by_sgrnascorer'] == CODE_ACCEPTED,
+            collection[target23]['passed_g20'] == CODE_ACCEPTED,
         ])
-
-        
+        collection[target23]['passed_crackling'] = CODE_ACCEPTED if collection[guide]['consensus_count'] >= 2 else CODE_REJECTED
