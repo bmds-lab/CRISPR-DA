833c445bfe85bad61206f9e2e5c881021db88c21
Carl Schmitz <carl.schmitz@connect.qut.edu.au>
2025-09-25 08:52:09 +1000

    Added some options for testing

--- examples/design_for_covid_spike_protein.py
+++ examples/design_for_covid_spike_protein.py
@@ -16,13 +16,32 @@ def main():
     number_viruses = 50
     gene_id = '43740568'
 
-    viruses = pd.read_csv('viruses.csv')
-    accessions = list(viruses['Assembly'].head(number_viruses))
+    # NOTE: This was used for testing purposes, Please update later
+    viruses = pd.read_csv('viruses_updated.csv')
+    accessions = list(viruses['Assembly Accession'].head(number_viruses))
+    output = run_analysis(target_gene_id=gene_id, evaluation_accessions=accessions)
+    g = []
+    a  = []
+    mit = []
+    cfd = []
+    total = []
+    unique = []
+    for accession in accessions:
+        for guide in output.guides:
+            if len(output.guides[guide].assembly_scores) > 0:
+                m, c, unique_sites, total_sites = output.guides[guide].assembly_scores[accession].values()
+                g.append(guide)
+                a.append(accession)
+                mit.append(m)
+                cfd.append(c)
+                total.append(total_sites)
+                unique.append(unique_sites)
 
-    gc, scores = run_analysis(target_gene_id=gene_id, evaluation_accessions=accessions)
-    
-    print(gc.to_dataframe())
-    print(pd.DataFrame(scores))
+    data = pd.DataFrame({'guide': g, 'accession': a, 'MIT score': mit, 'CFD score': cfd, 'total_sites': total, 'unique_sites': unique})
+    # data.to_csv('/mnt/ssd1/carl/ViralCut/issl_scores.csv', index=False)
+
+    # print(gc.to_dataframe())
+    # print(pd.DataFrame(scores))
 
 if __name__ == '__main__':
     main()
