ea63d9f99fc697c392408da797b8bb8e00b74293
Carl Schmitz <carl.schmitz@connect.qut.edu.au>
2025-12-09 17:11:40 +1000

    Restructed project

--- src/crispr_da/config.py
+++ src/crispr_da/config.py
@@ -4,7 +4,7 @@ from importlib import resources
 # NCBI accessions. This method parses the CSV file that is provided when clicking 'Download' on
 # this page https://www.ncbi.nlm.nih.gov/genome/browse/. Please ensure the 'Assembly' column is
 # present before exporting. 
-NCBI_HUMAN_VIRUSES_TABLE = resources.path('viralcut.resources', 'human_viruses.csv')
+NCBI_HUMAN_VIRUSES_TABLE = resources.path('crispr_da.resources', 'human_viruses.csv')
 
 # The directory to write files downloaded from NCBI to
 CACHE = './genomes-viruses'
